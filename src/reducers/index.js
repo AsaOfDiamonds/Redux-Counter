2d5122140d1be77e593b7fab344a3317c71d6d47
1@$@1github <asaofdiamonds@gmail.com>
2018-12-11 09:42:15 -0700

    could not get indux.js reducers to work, will show on page but not increment

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,9 +12,11 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+      return {
+        ...state,
+        count: ++state}
     case DECREMENT:
-    // Fill in the body of this case
+      return --state;
     default:
       return state;
   }
